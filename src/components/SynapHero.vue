591fc547dc8ac06554a8882e73797d2711ec4387
yuyudifiesh <yizhixianyuuuu@gmail.com>
2026-06-09 00:39:16 +0800

    update

--- src/components/SynapHero.vue
+++ src/components/SynapHero.vue
@@ -7,7 +7,7 @@
       <p class="subtitle">捕捉灵感 快如闪电</p>
       
       <div class="hero-buttons">
-        <router-link to="/SynapDownload" class="md-btn md-btn-filled">
+        <router-link to="/Download" class="md-btn md-btn-filled">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
             <polyline points="7 10 12 15 17 10"/>
